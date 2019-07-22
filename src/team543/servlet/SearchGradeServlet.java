23ae9647eb3b5cb9e9b3df57c905dbaf3db660c3
tomonori <cs596065766@hotmail.com>
2019-07-22 14:19:55 +0800

    add many Servlet

--- src/team543/servlet/SearchGradeServlet.java
+++ src/team543/servlet/SearchGradeServlet.java
@@ -7,15 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class SearchGradeServlet
  */
-public class UpdateServlet extends HttpServlet {
+public class SearchGradeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public SearchGradeServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -25,7 +25,6 @@ public class UpdateServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Update");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
