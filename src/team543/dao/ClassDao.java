2bb1ae61f5aef59eb82c6aea49ded937880f2419
oguratomonori <cs596065766@hotmail.com>
2019-07-22 14:24:35 +0800

    update dao

---
+++ src/team543/dao/ClassDao.java
@@ -0,0 +1,114 @@
+package team543.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ClassDao {
+	/**
+	 * @param c_id
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
+	 */
+	public void deleteClass(String c_id) throws ReflectiveOperationException, SQLException {
+		String sql = "DELETE FROM t_class WHERE c_id=?";
+		Connection connection = utils.DBUtils.getConnection();
+		PreparedStatement pst = connection.prepareStatement(sql);
+		pst.setString(1, c_id);
+		pst.executeUpdate();
+	}
+	
+	/**
+	 * @param c
+	 * @throws SQLException
+	 * @throws ReflectiveOperationException
+	 */
+	public void updateClass(entity.Class c) throws SQLException, ReflectiveOperationException {
+		String sql = "UPDATE t_class SET c_name=? , c_classState=? , c_score = ?, c_openDP=? , c_percentage=?  WHERE c_id=?";
+		Connection connection = utils.DBUtils.getConnection();
+		PreparedStatement pst = connection.prepareStatement(sql);
+		pst.setString(1, c.getC_name());
+		pst.setString(2, c.getC_classstate());
+		pst.setString(3, c.getC_source());
+		pst.setString(4, c.getC_opendp());
+		pst.setInt(5, c.getC_percentage());
+		pst.setString(6, c.getC_id());
+		pst.executeUpdate();
+		
+	}
+	
+	/**
+	 * @param c
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
+	 */
+	public void addClass(entity.Class c) throws ReflectiveOperationException, SQLException {
+		String sql="INSERT INTO t_giveclass(c_id,c_name,c_classState,c_score,c_openDP,c_percentage) VALUES(?,?,?,?,?,?)";
+		
+		Connection connection = utils.DBUtils.getConnection();
+		PreparedStatement pst = connection.prepareStatement(sql);
+		
+		pst.setString(1, c.getC_id());
+		pst.setString(2, c.getC_name());
+		pst.setString(3, c.getC_classstate());
+		pst.setString(4, c.getC_source());
+		pst.setString(5, c.getC_opendp());
+		pst.setInt(6, c.getC_percentage());
+		pst.executeUpdate();
+	}
+	/**
+	 * 输出课程id的课程信息
+	 * @param id
+	 * @throws SQLException 
+	 * @throws ReflectiveOperationException 
+	 */
+	public entity.Class getClassById(String id) throws ReflectiveOperationException, SQLException {
+		String sql = "SELECT * FROM t_class where t_id = '"+ id +"';";
+		
+		Connection connection = utils.DBUtils.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		entity.Class c = null;
+		
+		if(rs.next()) {
+			c.setC_id(rs.getString("c_id"));
+			c.setC_name(rs.getString("c_name"));
+			c.setC_classstate(rs.getString("c_classState"));
+			c.setC_source(rs.getString("c_source"));
+			c.setC_opendp(rs.getString("c_openDP"));
+			c.setC_percentage(rs.getInt("C_percentage"));
+			return c;
+		}
+		return c;
+	}
+	/**
+	 * @return List<entity.Class>类型
+	 * @throws SQLException
+	 * @throws ReflectiveOperationException
+	 */
+	public List<entity.Class> getAllClass() throws SQLException, ReflectiveOperationException{
+		String sql = "SELECT * FROM t_class ;";
+		
+		Connection connection = utils.DBUtils.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		List<entity.Class> cl = null;
+		
+		while(rs.next()) {
+			entity.Class c =new entity.Class();
+			c.setC_id(rs.getString("c_id"));
+			c.setC_name(rs.getString("c_name"));
+			c.setC_classstate(rs.getString("c_classState"));
+			c.setC_source(rs.getString("c_source"));
+			c.setC_opendp(rs.getString("c_openDP"));
+			c.setC_percentage(rs.getInt("C_percentage"));
+			cl.add(c);
+		}
+		return cl;
+	}
+}
---
+++ src/team543/dao/GiveClassDao.java
@@ -0,0 +1,120 @@
+package team543.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.GiveClass;
+
+
+public class GiveClassDao {
+	 /**
+	  * 删除授课信息
+	  *	 传入课程id，连接
+	  * @throws ReflectiveOperationException 
+	  *
+	  */
+		/**
+		 * @param c_id
+		 * @throws SQLException
+		 * @throws ReflectiveOperationException
+		 */
+	public void deleteGiveClass(String c_id) throws SQLException, ReflectiveOperationException {
+		String sql = "DELETE FROM t_giveclass WHERE c_id=?";
+		Connection connection = utils.DBUtils.getConnection();
+		PreparedStatement pst = connection.prepareStatement(sql);
+		pst.setString(1, c_id);
+		pst.executeUpdate();
+	}
+	
+	/**
+	 * @param giveclass
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
+	 */
+	public void updateGiveClass(GiveClass gc) throws ReflectiveOperationException, SQLException {
+		String sql="UPDATE t_giveclass SET t_id=?,c_time=?, t_stite=? WHERE c_id=?";
+		Connection connection = utils.DBUtils.getConnection();
+		
+		PreparedStatement pst = connection.prepareStatement(sql);
+		
+		pst.setString(1, gc.getT_id());
+		pst.setString(2, gc.getC_time());
+		pst.setString(3, gc.getT_stite());
+		pst.setString(4, gc.getC_id());
+		pst.executeUpdate();
+		
+	}
+	
+	/**
+	 * @param gc
+	 * @throws ReflectiveOperationException
+	 * @throws SQLException
+	 */
+	public void addGiveClass(GiveClass gc) throws ReflectiveOperationException, SQLException {
+		String sql="INSERT INTO t_giveclass(c_id,t_id,c_time,t_stite) VALUES(?,?,?,?)";
+		Connection connection = utils.DBUtils.getConnection();
+		
+		PreparedStatement pst = connection.prepareStatement(sql);
+		
+		pst.setString(1, gc.getC_id());
+		pst.setString(2, gc.getT_id());
+		pst.setString(3, gc.getC_time());
+		pst.setString(4, gc.getT_stite());
+		
+		pst.executeUpdate();
+		
+	}
+	
+	public List<GiveClass> getGiveClassById(String id) throws ReflectiveOperationException, SQLException{
+		//获取数据库连接
+		Connection connection = utils.DBUtils.getConnection();
+		
+		Statement statement = connection.createStatement();
+		
+		String sql = "SELECT * FROM t_giveclass where t_id = '"+ id +"';";
+		
+		ResultSet rs = statement.executeQuery(sql);
+		
+		List<GiveClass> giveclass = new ArrayList<GiveClass>();
+		
+		while(rs.next()) {
+			GiveClass gc = new GiveClass();
+			gc.setC_id(rs.getString("c_id"));
+			gc.setT_id(rs.getString("t_id"));
+			gc.setC_time(rs.getString("c_time"));
+			gc.setT_stite(rs.getString("t_stite"));
+			giveclass.add(gc);
+		}
+		return giveclass;
+	}
+	
+	
+	
+	public List<GiveClass> getAllGiveClass() throws ReflectiveOperationException, SQLException{
+		//获取数据库连接
+		Connection connection = utils.DBUtils.getConnection();
+		
+		Statement statement = connection.createStatement();
+		String sql = "SELECT * FROM t_giveclass;";
+		
+		ResultSet rs = statement.executeQuery(sql);
+		
+		List<GiveClass> giveclass = new ArrayList<GiveClass>();
+		
+		while(rs.next()) {
+			GiveClass gc = new GiveClass();
+			gc.setC_id(rs.getString("c_id"));
+			gc.setT_id(rs.getString("t_id"));
+			gc.setC_time(rs.getString("c_time"));
+			gc.setT_stite(rs.getString("t_stite"));
+			giveclass.add(gc);
+		}
+		return giveclass;
+	}
+
+}
