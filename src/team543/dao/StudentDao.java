9c6fdcb6e09ddb0b3788203675a764b11b7cf230
赵宇浩 <ZYH981121@outlook.com>
2019-07-22 15:38:41 +0800

    null

---
+++ src/team543/dao/StudentDao.java
@@ -0,0 +1,162 @@
+package team543.dao;
+
+import team543.entity.Student;
+import team543.utils.DBUtils;
+import team543.entity.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StudentDao {
+
+    /*
+     * 获取学生列表
+     * @return students(ArrayList)
+     * @throws ReflectiveOperationException
+     * @throws SQLException
+     */
+    public static List<Student> getStudents() throws ReflectiveOperationException, SQLException{
+        //获取数据库连接
+        Connection connection = DBUtils.getConnection();
+        //创建Statement
+        Statement statement = connection.createStatement();
+        //sql语句
+        String sql="SELECT * FROM t_student";
+        //执行sql语句
+        ResultSet rs = statement.executeQuery(sql);
+        List<Student> students = new ArrayList<Student> ();
+        while(rs.next()) {
+            Student student = new Student();
+            student.setS_id (rs.getString ("s_id"));
+            student.setS_name (rs.getString("s_name"));
+            student.setS_sex (rs.getString("s_sex"));
+            student.setS_dp (rs.getString ("s_dp"));
+            student.setS_major (rs.getString("s_major"));
+            student.setS_class (rs.getString ("s_class"));
+            student.setS_state (rs.getString("s_state"));
+            student.setEntertame (rs.getString("s_entertime"));
+            students.add(student);
+        }
+        DBUtils.closeConn();
+        return students;
+    }
+
+
+    /*
+     * 通过学生ID查询学生信息
+     * @param studentId
+     * @return student(class)
+     * @throws ReflectiveOperationException
+     * @throws SQLException
+     */
+    public static Student getStudentById (Integer studentId) throws ReflectiveOperationException, SQLException {
+        //获取数据库连接
+        Connection connection = DBUtils.getConnection();
+        //创建Statement
+        Statement statement = connection.createStatement();
+        //sql语句
+        String sql="SELECT * FROM t_student WHERE s_id="+studentId;
+        //执行sql语句
+        ResultSet rs = statement.executeQuery(sql);
+        if(rs.next()) {
+            Student student = new Student();
+            student.setS_id (rs.getString ("s_id"));
+            student.setS_name (rs.getString("s_name"));
+            student.setS_sex (rs.getString("s_sex"));
+            student.setS_dp (rs.getString ("s_dp"));
+            student.setS_major (rs.getString("s_major"));
+            student.setS_class (rs.getString  ("s_class"));
+            student.setS_state (rs.getString("s_state"));
+            student.setEntertame (rs.getString("s_entertime"));
+            return student;
+        }
+        return null;
+    }
+
+
+    /*
+     * 添加学生(批量)
+     * 学号、姓名、性别、系别、班级、状态、入学时间
+     * @param Student[]
+     * @throws SQLException
+     * @throws ReflectiveOperationException
+     */
+    public static void addStudent(Student[] s) throws SQLException, ReflectiveOperationException {
+        //获取数据库连接
+        Connection connection = DBUtils.getConnection();
+        //sql语句
+        for (Student student:s
+        ) {
+            String sql = "INSERT into  t_student (s_id, s_name, s_sex, s_dp, s_class, s_state, s_entertime) VALUES (?,?,?,?,?,?,?)";
+            //创建prepareStatement
+            PreparedStatement pst = connection.prepareStatement (sql);
+            pst.setString (1, student.getS_id ());
+            pst.setString (2, student.getS_name ());
+            pst.setString (3, student.getS_sex ());
+            pst.setString (4, student.getS_dp ());
+            pst.setString (5, student.getS_class ());
+            pst.setString (6, student.getS_state ());
+            pst.setString (7, student.getEntertame ());
+            pst.executeUpdate ();
+        }
+        team543.utils.DBUtils.closeConn();
+    }
+
+
+
+    /*
+     * 通过学生ID删除学生（批量）
+     * @param id[]
+     * @throws SQLException
+     * @throws ReflectiveOperationException
+     */
+    public static void deleteStudent(int[] id) throws SQLException, ReflectiveOperationException {
+        //获取数据库连接
+        Connection connection = DBUtils.getConnection();
+        for (int i=0;i<id.length;i++
+             ) {
+            //sql语句
+            String sql="DELETE FROM t_student WHERE s_id=?";
+            //创建prepareStatement
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, id[i]);
+            pst.executeUpdate();
+        }
+
+        DBUtils.closeConn();
+    }
+
+
+    /*
+     * 更新学生信息（批量操作）
+     * s_name,s_sex,s_dp,s_major,s_class,s_state,s_entertime
+     * @param  Student[]
+     * @throws SQLException
+     * @throws ReflectiveOperationException
+     */
+    public static void updateStudent (Student[] s) throws SQLException, ReflectiveOperationException {
+        //获取数据库连接
+        Connection connection = DBUtils.getConnection ();
+
+        for (Student student:s
+             ) {
+            String sql = "UPDATE t_student SET s_name=?,s_sex=?,s_dp=?,s_major=?,s_class=?,s_state=?,s_entertime=? WHERE s_id=?";
+            //创建prepareStatement
+            PreparedStatement pst = connection.prepareStatement (sql);
+            pst.setString (1, student.getS_name ());
+            pst.setString (2, student.getS_sex ());
+            pst.setString (3, student.getS_dp ());
+            pst.setString (4, student.getS_major ());
+            pst.setString (5, student.getS_class ());
+            pst.setString (6, student.getS_state ());
+            pst.setString (7, student.getEntertame ());
+            pst.setString (8, student.getS_id ());
+            pst.executeUpdate ();
+        }
+
+        //sql语句
+        DBUtils.closeConn ();
+    }
+}
---
+++ src/team543/test/StudentDaoTest.java
@@ -0,0 +1,76 @@
+package team543.test;
+
+import team543.dao.StudentDao;
+import team543.entity.Student;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentDaoTest {
+
+    private StudentDao studentDao = new StudentDao ();
+
+    @Test
+    public void TestGetStudentByIdDao() throws ReflectiveOperationException, SQLException {
+
+        int id = 20170;
+        Student student = StudentDao.getStudentById(id);
+        System.out.println (student.toString ());
+
+    }
+
+    @Test
+    public void testGetStudents () throws ReflectiveOperationException, SQLException {
+        List<Student> students = studentDao.getStudents ();
+        for (int i = 0; i < students.size (); i++) {
+
+            Student student = students.get (i);
+            student.toString ();
+
+        }
+        System.out.println(students);
+    }
+
+    @Test
+    public void testUpdateStudent() throws ReflectiveOperationException, SQLException{
+        Student a = new Student ();
+        a.setS_id ("1212");
+        a.setS_name ("b");
+        a.setS_sex ("b");
+        a.setS_dp ("bb");
+        a.setS_major ("b");
+        a.setS_class ("121");
+        a.setS_state ("c");
+        a.setEntertame (null);
+        Student[] s = {a};
+        StudentDao.updateStudent (s);
+    }
+
+    @Test
+    public void testAddStudent() throws ReflectiveOperationException, SQLException{
+        Student a = new Student ();
+        a.setS_id ("123");
+        a.setS_name ("b");
+        a.setS_sex ("b");
+        a.setS_dp ("bb");
+        a.setS_major ("b");
+        a.setS_class ("12");
+        a.setS_state ("c");
+        a.setEntertame (null);
+        Student[] s = {a};
+        StudentDao.addStudent (s);
+
+    }
+
+    @Test
+    public void testDeleteStudent() throws ReflectiveOperationException, SQLException{
+        int[] id = {2017061226,2017061207};
+        StudentDao.deleteStudent (id);
+    }
+
+
+}
+	
+	
+
