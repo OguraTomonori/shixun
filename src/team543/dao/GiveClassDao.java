8bda9752787af54af1fc9cff38dc5b8385147443
赵宇浩 <ZYH981121@outlook.com>
2019-07-22 16:38:09 +0800

    null

--- src/team543/dao/GiveClassDao.java
+++ src/team543/dao/GiveClassDao.java
@@ -9,69 +9,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 import team543.entity.GiveClass;
+import team543.utils.DBUtils;
 
 
 public class GiveClassDao {
-	 /**
-	  * ɾ���ڿ���Ϣ
-	  *	 ����γ�id������
+	 /*
+	  * ?????????
+	  *	 ????��?id??????
 	  * @throws ReflectiveOperationException 
 	  *
 	  */
-		/**
+		/*
 		 * @param c_id
 		 * @throws SQLException
 		 * @throws ReflectiveOperationException
 		 */
 	public void deleteGiveClass(String c_id) throws SQLException, ReflectiveOperationException {
-		String sql = "DELETE FROM t_giveclass WHERE c_id=?";
 		Connection connection = team543.utils.DBUtils.getConnection();
+		String sql = "DELETE FROM t_giveclass WHERE c_id=?";
 		PreparedStatement pst = connection.prepareStatement(sql);
 		pst.setString(1, c_id);
 		pst.executeUpdate();
+		DBUtils.closeConn();
 	}
 	
-	/**
+	/*
 	 * @param giveclass
 	 * @throws ReflectiveOperationException
 	 * @throws SQLException
 	 */
 	public void updateGiveClass(GiveClass gc) throws ReflectiveOperationException, SQLException {
-		String sql="UPDATE t_giveclass SET t_id=?,c_time=?, t_stite=? WHERE c_id=?";
+
 		Connection connection = team543.utils.DBUtils.getConnection();
-		
+		String sql="UPDATE t_giveclass SET t_id=?,c_time=?, t_site=? WHERE c_id=?";
 		PreparedStatement pst = connection.prepareStatement(sql);
-		
 		pst.setString(1, gc.getT_id());
 		pst.setString(2, gc.getC_time());
-		pst.setString(3, gc.getT_state());
+		pst.setString(3, gc.getT_site ());
 		pst.setString(4, gc.getC_id());
 		pst.executeUpdate();
-		
+		DBUtils.closeConn();
 	}
 	
-	/**
+	/*
 	 * @param gc
 	 * @throws ReflectiveOperationException
 	 * @throws SQLException
 	 */
 	public void addGiveClass(GiveClass gc) throws ReflectiveOperationException, SQLException {
-		String sql="INSERT INTO t_giveclass(c_id,t_id,c_time,t_stite) VALUES(?,?,?,?)";
+
 		Connection connection = team543.utils.DBUtils.getConnection();
-		
+		String sql="INSERT INTO t_giveclass(c_id,t_id,c_time,t_site) VALUES(?,?,?,?)";
 		PreparedStatement pst = connection.prepareStatement(sql);
-		
 		pst.setString(1, gc.getC_id());
 		pst.setString(2, gc.getT_id());
 		pst.setString(3, gc.getC_time());
-		pst.setString(4, gc.getT_state());
-		
+		pst.setString(4, gc.getT_site ());
 		pst.executeUpdate();
-		
+		DBUtils.closeConn();
 	}
 	
 	public List<GiveClass> getGiveClassById(String id) throws ReflectiveOperationException, SQLException{
-		//��ȡ���ݿ�����
+		//????????????
 		Connection connection = team543.utils.DBUtils.getConnection();
 		
 		Statement statement = connection.createStatement();
@@ -87,7 +86,7 @@ public class GiveClassDao {
 			gc.setC_id(rs.getString("c_id"));
 			gc.setT_id(rs.getString("t_id"));
 			gc.setC_time(rs.getString("c_time"));
-			gc.setT_state(rs.getString("t_stite"));
+			gc.setT_site (rs.getString("t_site"));
 			giveclass.add(gc);
 		}
 		return giveclass;
@@ -96,22 +95,18 @@ public class GiveClassDao {
 	
 	
 	public List<GiveClass> getAllGiveClass() throws ReflectiveOperationException, SQLException{
-		//��ȡ���ݿ�����
+		//????????????
 		Connection connection = team543.utils.DBUtils.getConnection();
-		
 		Statement statement = connection.createStatement();
 		String sql = "SELECT * FROM t_giveclass;";
-		
 		ResultSet rs = statement.executeQuery(sql);
-		
 		List<GiveClass> giveclass = new ArrayList<GiveClass>();
-		
 		while(rs.next()) {
 			GiveClass gc = new GiveClass();
 			gc.setC_id(rs.getString("c_id"));
 			gc.setT_id(rs.getString("t_id"));
 			gc.setC_time(rs.getString("c_time"));
-			gc.setT_state(rs.getString("t_stite"));
+			gc.setT_site (rs.getString("t_site"));
 			giveclass.add(gc);
 		}
 		return giveclass;
